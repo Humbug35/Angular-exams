b772d2ebf1b7a2cc961c0ef8f3a6f9b6a91ddf50
Christoffer <jo.christoffer@gmail.com>
2018-05-31 10:27:54 +0200

    skapat upp component och services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
