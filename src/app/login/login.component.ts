a229ad1e12baa8a3c1771ac95a0d15aa352500b6
Christoffer <jo.christoffer@gmail.com>
2018-05-31 13:39:20 +0200

    lagt till form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userInput = {
+    username: '',
+    password: ''
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login() {
+    console.log(this.userInput);
+  }
+
+
 }
+
