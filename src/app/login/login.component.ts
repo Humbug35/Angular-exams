2b84c70acc5e02153bcf69eb4be5fb69276a0f87
Malin Hanak <malinehanak@gmail.com>
2018-06-07 10:10:54 +0200

    accessing and saving token form dropbox

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router, CanActivate } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,19 +15,30 @@ export class LoginComponent implements OnInit {
   };
   failedLoggedIn = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router, private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activateRoute.url.subscribe(() => {
+      const responsUrl = this.router.url;
+      if(responsUrl.indexOf('&') !== -1) {
+      const params = responsUrl.split('&')
+      console.error('token now', params);
+      const authToken = params[0].split('=');
+      console.error('token now', authToken);
+      const myToken = authToken[1]
+      console.error('mytoken ', myToken)
+
+      localStorage.setItem('token', myToken);
+      this.router.navigate(['/home'])
+
+      } else { this.router.navigate(['/login']) }
+    })
   }
 
   login() {
-    const authURL = "https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=nyfmqnlf5r5ndgz&redirect_uri=http://localhost:4200/home";
+    const authURL = "https://www.dropbox.com/oauth2/authorize?response_type=token&client_id=nyfmqnlf5r5ndgz&redirect_uri=http://localhost:4200/login";
     return window.location.href = authURL;
   }
-
-  /*logout() {
-    this.authService.logout();
-  }*/
 
 }
 
